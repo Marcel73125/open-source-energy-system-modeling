819f7c869ae623b7c53ab931a89dd313c6f76853
Marcel73125 <163716765+Marcel73125@users.noreply.github.com>
2024-03-24 09:53:44 +0100

    Update test_utils.py

--- test_utils.py
+++ test_utils.py
@@ -1,9 +1,9 @@
 from utils import glucoselevel
-from sympy import symbols, dsolve, Eq, Function
-from IPython.display import display
-
 
 def test_glucoselevel():
+    from sympy import symbols, dsolve, Eq, Function
+    from IPython.display import display
+    
     assert glucoselevel(80, 0) == 80
     assert glucoselevel(80, 60) == 61.0704
     assert glucoselevel(-1, 1) == -1
