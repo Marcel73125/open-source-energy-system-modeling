425cc4d5f1e483bbdc1da29a2ea00a237ec5dd1f
Marcel73125 <163716765+Marcel73125@users.noreply.github.com>
2024-03-24 10:11:59 +0100

    Remove Sympy imports from test_utils.py

--- test_utils.py
+++ test_utils.py
@@ -1,8 +1,6 @@
 from utils import glucoselevel
 
 def test_glucoselevel():
-    from sympy import symbols, dsolve, Eq, Function
-    from IPython.display import display
     
     assert glucoselevel(80, 0) == 80
     assert glucoselevel(80, 60) == 61.0704
