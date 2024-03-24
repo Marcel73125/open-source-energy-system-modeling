d4baf844fe0a682ed8cd564d220cd263259d1361
Marcel73125 <163716765+Marcel73125@users.noreply.github.com>
2024-03-24 18:46:15 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -23,7 +23,7 @@ def glucoselevel(g_init, time): #g_init = initial glucose level in mg/dl;  #time
 #the function printglucoselevel calculates the glucose level in mg/dl at the handed over time in hours with a modeled threshold of 70mg/dl 
 # at the given initial glucose level and plots it in the interval [0,time]
 # returns 0 if inputs were valid; # returns -1 if either g_init or time_hours is negative
-def printglucoselevel(g_init, time_hours):
+def printglucoselevel(g_init, time_hours): #g_init = initial glucose level in mg/dl;  #time_hours in hours
     if g_init >= 0: #checks if value for g_init is valid
         #solving differential equation with given initial glucose level; (g(t)) - 70) models threshold at which level is dangerously low for humans;
         #ics sets the initial condition; #[g] = mg/dl
@@ -32,7 +32,7 @@ def printglucoselevel(g_init, time_hours):
 
         if time_hours >= 0:
             #solving differential equation with modeled threshold at given time value
-            result_value_2 = sol_2.subs({k: k_value, t: time_hours}) # .subs substitutes the symbols k with the defined k_value and t with handed over time value
+            result_value_2 = sol_2.subs({k: k_value, t: time_hours}) # .subs substitutes the symbols k with the defined k_value and t with handed over time value in hours
             display(result_value_2) #print solution at given time value;   #example: g_init=80, k=0.0045, t=10,  g(10)=79.559974818331
 
             plot(sol_2.rhs.subs({k: k_value}), (t, 0, time_hours*60), xlabel='t in [min]', ylabel='Glucoselevel in [mg/dl]',axis_center=(0,70.5)) #plots glucose level in the interval [0,time]
