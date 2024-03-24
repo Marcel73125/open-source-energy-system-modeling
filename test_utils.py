4aae07839b3f21bbeb2075d24c1ea958f89fd4a0
Marcel73125 <163716765+Marcel73125@users.noreply.github.com>
2024-03-24 14:43:19 +0100

    Add test module for printglucoselevel() from utils.py

--- test_utils.py
+++ test_utils.py
@@ -1,6 +1,11 @@
 from utils import glucoselevel
+from utils import printglucoselevel
 
 def test_glucoselevel():
     
     assert glucoselevel(80, 0) == 80
     assert glucoselevel(-1, 1) == -1
+
+def test_printglucoselevel():
+    assert printglucoselevel(80, 2) == 0
+    assert printglucoselevel(80, -10) == -1
