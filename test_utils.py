cb5594d4eefc57a57e34cb593ef03a581aea7744
sebastiansuer <163882125+sebastiansuer@users.noreply.github.com>
2024-04-22 15:46:00 +0200

    Update test_utils.py; make tests fail

--- test_utils.py
+++ test_utils.py
@@ -5,6 +5,7 @@ def test_glucoselevel():
     
     assert glucoselevel(80, 0) == 80
     assert glucoselevel(-1, 1) == -1
+    assert glucoselevel  (70,0) == 80
 
 def test_printglucoselevel():
     assert printglucoselevel(80, 2) == 0
